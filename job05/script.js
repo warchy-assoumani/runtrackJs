ae6958afd4a137742becd1592a9a0e4d83221f8d
warchy-assoumani <warchy.assoumani@laplateforme.io>
2026-04-21 19:56:10 +0200

    Add files via upload
    fonction “afficherJoursSemaines” sans paramètre. 
    affiche les jours de la semaine à l'aide d’une boucle for un à un

---
+++ job05/script.js
@@ -0,0 +1,8 @@
+function afficherJoursSemaines() {
+    const jours = ["lundi", "mardi", "mercredi", "jeudi",
+                    "vendredi", "samedi", "dimanche" ];
+    for (let i = 0; i < jours.length; i++) {
+        console.log(jours[i]);
+    }
+}
+afficherJoursSemaines()
